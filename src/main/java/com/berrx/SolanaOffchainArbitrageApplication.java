f72de708cd89ccd8012ec75af5fa7fd433366da8
qajybai <qajybai@gmail.com>
2025-09-13 10:38:30 +0500

    init commit

---
+++ src/main/java/com/berrx/SolanaOffchainArbitrageApplication.java
@@ -0,0 +1,13 @@
+package com.berrx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolanaOffchainArbitrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolanaOffchainArbitrageApplication.class, args);
+	}
+
+}
